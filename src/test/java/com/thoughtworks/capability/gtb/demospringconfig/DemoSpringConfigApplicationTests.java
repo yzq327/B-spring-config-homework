a27daebc120f0911a18d5d97dca21b74a7d52eab
Yang Zhengqing <1370483456@qq.com>
2020-11-17 23:54:55 +0800

    finish p2s1

--- src/test/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplicationTests.java
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplicationTests.java
@@ -1,8 +1,14 @@
 package com.thoughtworks.capability.gtb.demospringconfig;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class DemoSpringConfigApplicationTests {
 
@@ -10,4 +16,14 @@ class DemoSpringConfigApplicationTests {
 	void contextLoads() {
 	}
 
+
+	@Autowired
+	private LevelController levelController;
+
+	@Test
+	public void shouldTestLevelController_overridePropertyValues() {
+		String levelNumber = levelController.getLevel();
+		assertEquals("advanced", levelNumber);
+
+	}
 }
