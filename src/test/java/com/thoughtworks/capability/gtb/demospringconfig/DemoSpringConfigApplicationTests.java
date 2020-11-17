957327879bf2b563f53fa76cdfd1942593f14f3b
Yang Zhengqing <1370483456@qq.com>
2020-11-17 23:58:30 +0800

    finish p2s2

--- src/test/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplicationTests.java
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplicationTests.java
@@ -4,18 +4,15 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 class DemoSpringConfigApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 
 	@Autowired
 	private LevelController levelController;
