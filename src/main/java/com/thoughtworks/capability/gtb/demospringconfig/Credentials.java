5e212c1d289c67b0129571794989c1ce3fa3432a
Yang Zhengqing <1370483456@qq.com>
2020-11-17 23:37:02 +0800

    finish p1si

---
+++ src/main/java/com/thoughtworks/capability/gtb/demospringconfig/Credentials.java
@@ -0,0 +1,32 @@
+package com.thoughtworks.capability.gtb.demospringconfig;
+
+public class Credentials {
+    private String authMethod;
+    private String username;
+    private String password;
+
+    public String getAuthMethod() {
+        return authMethod;
+    }
+
+    public void setAuthMethod(String authMethod) {
+        this.authMethod = authMethod;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
+
--- src/main/java/com/thoughtworks/capability/gtb/demospringconfig/MailConfig.java
+++ src/main/java/com/thoughtworks/capability/gtb/demospringconfig/MailConfig.java
@@ -2,12 +2,20 @@ package com.thoughtworks.capability.gtb.demospringconfig;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+
+import java.util.List;
+import java.util.Map;
+
 @ConfigurationProperties("mail")
 public class MailConfig {
 
     private String hostname;
     private int port;
     private String from;
+    private List<String> defaultRecipients;
+    private Map<String, String> additionalHeaders;
+    private Credentials credentials;
+
 
     public String getHostname() {
         return hostname;
@@ -33,12 +41,42 @@ public class MailConfig {
         this.from = from;
     }
 
+    public void setDefaultRecipients(List defaultRecipients) {
+        this.defaultRecipients = defaultRecipients;
+    }
+
+    public void setAdditionalHeaders(Map additionalHeaders) {
+        this.additionalHeaders = additionalHeaders;
+    }
+
+    public void setCredentials(Credentials credentials) {
+        this.credentials = credentials;
+    }
+
+    public List getDefaultRecipients() {
+        return defaultRecipients;
+    }
+
+    public Map getAdditionalHeaders() {
+        return additionalHeaders;
+    }
+
+    public Credentials getCredentials() {
+        return credentials;
+    }
+
     @Override
     public String toString() {
         return "MailConfig{" +
                 "hostname='" + hostname + '\'' +
                 ", port=" + port +
                 ", from='" + from + '\'' +
-                '}';
+                ", defaultRecipients[0]=" + defaultRecipients.get(0) +
+                ", defaultRecipients[1]=" + defaultRecipients.get(1) +
+                ", additionalHeaders=" + additionalHeaders +
+                ", credentials.getUsername=" + credentials.getUsername() +
+                ", credentials.getPassword=" + credentials.getPassword() +
+                ", credentials.getAuthMethod=" + credentials.getAuthMethod() +
+                '}' ;
     }
 }
